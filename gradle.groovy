083681dc0dd08dfa67491a1151106659ad875e96
mvermir <mvermir@gmail.com>
2020-12-16 17:50:21 -0300

    Update gradle.groovy

--- gradle.groovy
+++ gradle.groovy
@@ -22,7 +22,7 @@ def call(){
                     }
                     stage('run')
                     {
-                        bat "nohup start gradlew bootRun & >dev/null"
+                        bat "nohup start gradlew bootRun &"
                         sleep 20
 
                     }
